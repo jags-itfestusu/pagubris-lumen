dd1657483a462425f8bc2670f6718c203f92db60
fikrimus35 <mustofa13.fikri@gmail.com>
2021-04-06 06:51:36 +0700

    Fix CORS problem
    Signed-off-by: fikrimus35 <mustofa13.fikri@gmail.com>

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $allowedOrigins = explode(",", env("CORS_ORIGINS"));
+        $acceptedOrigin = "";
+
+        $originRequest = $request->header('Origin');
+        if (in_array($originRequest, $allowedOrigins)) {
+            $acceptedOrigin = $originRequest;
+        }
+
+        $headers = [
+            'Access-Control-Allow-Origin'      => $acceptedOrigin,
+            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age'           => '86400',
+            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
+        ];
+
+        if ($request->isMethod('OPTIONS')) {
+            return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        }
+
+        $response = $next($request);
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
+        }
+
+        return $response;
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -72,9 +72,9 @@ $app->configure('app');
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    \App\Http\Middleware\CorsMiddleware::class
+]);
 
 $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
@@ -92,6 +92,8 @@ $app->routeMiddleware([
 */
 
 // $app->register(App\Providers\AppServiceProvider::class);
+
+
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(\Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
